2e1bfd24296070502eaa43ac1ba45ea60aa66b02
Augusto Modesto <augusto.modesto@live.com>
2015-04-12 22:39:12 -0300

    Refining the model

--- Finpy/models.py
+++ Finpy/models.py
@@ -6,15 +6,43 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Periodicity(models.Model):
+
+    """Classe responsável por representar a periodicidade de uma receita/despesa"""
+    """In Portuguese: Periodicidade"""
+
+    DAILY = _('Daily') #Diário
+    WEEKLY = _('Weekly') #Semanal
+    MONTHLY = _('Monthly') #Mensal
+
+    periodicityID = models.AutoField(('Periodicity Identifier'), primary_key=True)
+    period = models.CharField(('Period Type'), choices=())
+
+class Accounting(models.Model):
+
+    """Classe responsável por manter os dados em comuns de receitas e despesas"""
+    """In Portuguese: Contabil"""
+
+
+
 class Income(models.Model):
 
+    """Classe que representa uma receita cadastrada pelo usuário"""
+    """In Portuguese: Receita"""
+
+    incomeID = models.AutoField(('Income Identifier'), primary_key=True)
+    dueDate = models.DateField(('Due Date'), editable=True, blank=True)
     salary = models.DecimalField('Salary', max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return str(self.salary)
+    def __str_(self):
+        return str(self.income)
 
 
 class UserProfile(models.Model):
+
+    """Classe que representa o perfil de um usuário"""
+    """In Portuguese: Perfil de Usuário"""
+
     user = models.OneToOneField(User)
 
     DF = 'DF'
@@ -47,12 +75,19 @@ class UserProfile(models.Model):
 
 class CategoryRecord(models.Model):
 
+    """Classe responsável por registrar uma categoria de receita e despesa"""
     """Class responsible for registering the registry of revenues and expenses"""
     """In Portuguese: Categoria de Lançamento"""
     
+    codyCategory = models.AutoField(primary_key=True);
+        #Primary Key of class. Automatically Generated
+
     nameCategory = models.CharField(_('nameCategory'), max_length=30, blank=False)
         #Name of category recorded    
         #Name of category never should be blank
+
+    pubDateCategory = models.DateTimeField('datePublishedCategory')
+        #Date of publication of category
         
     description = models.CharField(_('nameCategory'), max_length=150, blank=True)
         #Detail of category recorded
