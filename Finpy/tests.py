2b1844582d963f5e9c31d779b8b963a084e605fd
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2015-05-25 02:10:04 -0300

    Adding more tests

--- Finpy/tests.py
+++ Finpy/tests.py
@@ -4,9 +4,17 @@ from django.test import TestCase
 
 class FinpyViewsTestCase(TestCase):
 	def test_login(self):
-		response_index = self.client.get('/finpy/login/')
-		self.assertEqual(response_index.status_code, 200)
+		response_login = self.client.get('/finpy/login/')
+		self.assertEqual(response_login.status_code, 200)
 
 	def test_signup(self):
-		response_update_profile = self.client.get('/finpy/signup/')
-		self.assertEqual(response_update_profile.status_code, 200)
+		response_signup = self.client.get('/finpy/signup/')
+		self.assertEqual(response_signup.status_code, 200)
+
+	def test_entry_create(self):
+		response_entry_create = self.client.get('/finpy/entry/create/')
+		self.assertEqual(response_entry_create.status_code, 200)
+
+	def test_entry_list(self):
+		response_entry_list = self.client.get('/finpy/entry/list/')
+		self.assertEqual(response_entry_list.status_code, 200)
