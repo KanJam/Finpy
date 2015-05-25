5612b6454d3ebf6e358bab20b253facb8bd7da9d
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2015-05-25 01:52:43 -0300

    Adding initial tests for views

--- Finpy/tests.py
+++ Finpy/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class FinpyViewsTestCase(TestCase):
+	def test_login(self):
+		response_index = self.client.get('/finpy/login/')
+		self.assertEqual(response_index.status_code, 200)
+
+	def test_signup(self):
+		response_update_profile = self.client.get('/finpy/signup/')
+		self.assertEqual(response_update_profile.status_code, 200)
